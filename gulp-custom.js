587223762d66a6d1bad9c6942dc6cba3d4e89044
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-23 06:28:18 +0530

    Add files via upload

---
+++ gulp-custom.js
@@ -0,0 +1,5 @@
+let $gulp = require( 'gulp' );
+
+$gulp.task( 'test', () => {
+	console.log( 'Hello World \n' );
+} );
--- gulpfile.js
+++ gulpfile.js
@@ -1,130 +1,139 @@
 const $vs_boilerplate_v = "1.0.0";
-const $gulp             = require( 'gulp' );
-const $util             = require( 'gulp-util' );
-const $scss             = require( 'gulp-sass' );
-const $notify           = require( 'gulp-notify' );
-const $runSequence      = require( 'run-sequence' );
-const $minify_css       = require( 'gulp-clean-css' );
-const $concat           = require( 'gulp-concat' );
-const $autoprefixer     = require( 'gulp-autoprefixer' );
-const $sourcemaps       = require( 'gulp-sourcemaps' );
-const $webpack          = require( 'webpack-stream' );
-const $combine_files    = require( 'gulp-combine-files' );
-const $path             = require( 'path' );
-const $config           = require( './config.js' );
-const $defaults_config  = $config.default_config;
-const isObject          = obj => obj === Object( obj );
-const $cwd              = ( $full_path ) => $path.relative( process.cwd(), $full_path );
-const isUndefined       = val => val === undefined;
-const vs_config_value   = function( $array, $src, $dest ) {
-	if( typeof $dest === "undefined" || $dest === '' ) {
-		let $_src;
-		for( $_src in $array ) {
-			if( $path.normalize( $path.format( $path.parse( $_src ) ) ) === $src || $src === $_src ) {
-				$dest = $array[ $_src ];
-				break;
-			}
-		}
-	}
-	return $dest;
-};
-const $notice           = ( $notice ) => $gulp.src( '' ).pipe( $notify( $notice ) );
+
+const $gulp          = require( 'gulp' ),
+	  $util          = require( 'gulp-util' ),
+	  $scss          = require( 'gulp-sass' ),
+	  $notify        = require( 'gulp-notify' ),
+	  $runSequence   = require( 'run-sequence' ),
+	  $minify_css    = require( 'gulp-clean-css' ),
+	  $concat        = require( 'gulp-concat' ),
+	  $autoprefixer  = require( 'gulp-autoprefixer' ),
+	  $sourcemaps    = require( 'gulp-sourcemaps' ),
+	  $webpack       = require( 'webpack-stream' ),
+	  $combine_files = require( 'gulp-combine-files' ),
+	  $path          = require( 'path' ),
+	  $config        = require( './config.js' );
+
+try {
+	const $custom_gulp = require( './gulp-custom.js' );
+} catch( e ) {
+
+}
+
+const $defaults_config = $config.default_config,
+	  isObject         = obj => obj === Object( obj ),
+	  $cwd             = ( $full_path ) => $path.relative( process.cwd(), $full_path ),
+	  isUndefined      = val => val === undefined,
+	  vs_config_value  = function( $array, $src, $dest ) {
+		  if( typeof $dest === "undefined" || $dest === '' ) {
+			  let $_src;
+			  for( $_src in $array ) {
+				  if( $path.normalize( $path.format( $path.parse( $_src ) ) ) === $src || $src === $_src ) {
+					  $dest = $array[ $_src ];
+					  break;
+				  }
+			  }
+		  }
+		  return $dest;
+	  },
+	  $notice          = ( $notice ) => $gulp.src( '' ).pipe( $notify( $notice ) );
 
 const vs_file_option      = ( $src, $data, $key, $defaults ) => {
-	let $return = { src: $src, options: $defaults, dist: $data };
+		  let $return = { src: $src, options: $defaults, dist: $data };
 
-	if( false === isUndefined( $data ) && true === isObject( $data ) ) {
-		$return.dist = $data.dist;
-	} else if( false === isUndefined( $data ) && false === isObject( $data ) && false !== $data ) {
-		$return.dist = $data;
-	}
+		  if( false === isUndefined( $data ) && true === isObject( $data ) ) {
+			  $return.dist = $data.dist;
+		  } else if( false === isUndefined( $data ) && false === isObject( $data ) && false !== $data ) {
+			  $return.dist = $data;
+		  }
 
-	if( false === isUndefined( $data ) ) {
-		if( isObject( $data ) ) {
-			if( false === isUndefined( $data[ $key ] ) ) {
-				let $_d = $data[ $key ];
-				if( false === $_d ) {
-					$return.options = false;
-				} else {
-					if( true === $_d ) {
-						$_d = {};
-					} else if( false === $_d ) {
-						$_d = false;
-					}
+		  if( false === isUndefined( $data ) ) {
+			  if( isObject( $data ) ) {
+				  if( false === isUndefined( $data[ $key ] ) ) {
+					  let $_d = $data[ $key ];
+					  if( false === $_d ) {
+						  $return.options = false;
+					  } else {
+						  if( true === $_d ) {
+							  $_d = {};
+						  } else if( false === $_d ) {
+							  $_d = false;
+						  }
 
-					let $dist = $return.dist;
-					if( false === isUndefined( $_d.dist ) ) {
-						$dist = $_d.dist;
-						delete $_d[ 'dist' ];
-					}
+						  let $dist = $return.dist;
+						  if( false === isUndefined( $_d.dist ) ) {
+							  $dist = $_d.dist;
+							  delete $_d[ 'dist' ];
+						  }
 
-					if( false === isUndefined( $_d.src ) ) {
-						$return.src = $_d.src;
-						delete $_d[ 'src' ];
-					}
-					let $k;
-					for( $k in $defaults ) {
-						if( isUndefined( $_d[ $k ] ) ) {
-							$_d[ $k ] = $defaults[ $k ];
-						}
-					}
+						  if( false === isUndefined( $_d.src ) ) {
+							  $return.src = $_d.src;
+							  delete $_d[ 'src' ];
+						  }
+						  let $k;
+						  for( $k in $defaults ) {
+							  if( isUndefined( $_d[ $k ] ) ) {
+								  $_d[ $k ] = $defaults[ $k ];
+							  }
+						  }
 
-					$return.options = $_d;
-					$return.dist    = $dist;
-				}
-			} else {
-				//$return.options = false;
-			}
-		}
-	}
+						  $return.options = $_d;
+						  $return.dist    = $dist;
+					  }
+				  } else {
+					  //$return.options = false;
+				  }
+			  }
+		  }
 
-	return $return;
-};
-const vs_compile_scss     = function( $_path ) {
-	let $instance = new VS_Gulp( $config.scss, $_path, vs_config_value( $config.scss, $_path ) );
-	$instance.sourcemap();
-	$instance.combine_files();
-	$instance.scss();
-	$instance.autoprefixer();
-	$instance.concat();
-	$instance.minify();
-	$instance.sourcemap( true );
-	$instance.save();
-	return $instance;
-};
-const vs_compile_js       = function( $_path ) {
-	let $instance = new VS_Gulp( $config.js, $_path, vs_config_value( $config.js, $_path ) );
-	$instance.combine_files().webpack();
-	$instance.save();
-	return $instance;
-};
-const vs_compile_all_scss = function( $current_loop ) {
-	const $scss_files_keys = Object.keys( $config.scss );
-	if( false === isUndefined( $scss_files_keys[ $current_loop ] ) ) {
-		vs_compile_scss( $scss_files_keys[ $current_loop ] );
-		vs_compile_all_scss( $current_loop + 1 );
-	}
-};
-const vs_compile_all_js   = function( $current_loop ) {
-	const $js_files_keys = Object.keys( $config.js );
-	if( false === isUndefined( $js_files_keys[ $current_loop ] ) ) {
-		vs_compile_js( $js_files_keys[ $current_loop ] );
-		vs_compile_all_js( $current_loop + 1 );
-	}
-};
+		  return $return;
+	  },
+	  vs_compile_scss     = function( $_path, $show_alert ) {
+		  let $instance = new VS_Gulp( $config.scss, $_path, vs_config_value( $config.scss, $_path ), $show_alert );
+		  $instance.sourcemap();
+		  $instance.combine_files();
+		  $instance.scss();
+		  $instance.autoprefixer();
+		  $instance.concat();
+		  $instance.minify();
+		  $instance.sourcemap( true );
+		  $instance.save();
+		  return $instance;
+	  },
+	  vs_compile_js       = function( $_path, $show_alert ) {
+		  let $instance = new VS_Gulp( $config.js, $_path, vs_config_value( $config.js, $_path ), $show_alert );
+		  $instance.combine_files().webpack().concat();
+		  $instance.save();
+		  return $instance;
+	  },
+	  vs_compile_all_scss = function( $current_loop ) {
+		  const $scss_files_keys = Object.keys( $config.scss );
+		  if( false === isUndefined( $scss_files_keys[ $current_loop ] ) ) {
+			  vs_compile_scss( $scss_files_keys[ $current_loop ], true )
+			  vs_compile_all_scss( $current_loop + 1 );
+		  }
+	  },
+	  vs_compile_all_js   = function( $current_loop ) {
+		  const $js_files_keys = Object.keys( $config.js );
+		  if( false === isUndefined( $js_files_keys[ $current_loop ] ) ) {
+			  vs_compile_js( $js_files_keys[ $current_loop ], true );
+			  vs_compile_all_js( $current_loop + 1 );
+		  }
+	  },
+	  VS_Gulp             = function( $vars, $src, $dist, $show_alert ) {
+		  this.src      = $src;
+		  this.dist     = $dist;
+		  this.notices  = '';
+		  this.vars     = $vars;
+		  this.instance = $gulp.src( this.src );
+		  this.alert    = $show_alert;
+	  };
 
-const VS_Gulp                   = function( $vars, $src, $dist ) {
-	this.src      = $src;
-	this.dist     = $dist;
-	this.notices  = '';
-	this.vars     = $vars;
-	this.instance = $gulp.src( this.src );
-};
 VS_Gulp.prototype.gulp_instance = function() {
 	return this.instance;
 };
 VS_Gulp.prototype.notice        = function( $notice ) {
-	this.notices += '✓ ' + $notice + '\r';
+	this.notices += '✓ ' + $notice + '\n';
 };
 VS_Gulp.prototype.option        = function( $key, $defaults ) {
 	return vs_file_option( this.src, this.dist, $key, $defaults );
@@ -214,7 +223,10 @@ VS_Gulp.prototype.webpack       = function() {
 VS_Gulp.prototype.save          = function() {
 	let $dest   = ( isObject( this.dist ) ) ? this.dist.dist : this.dist;
 	let $return = this.instance.pipe( $gulp.dest( $dest ) );
-	$notice( { title: $config.project_name + ' - ' + this.src, message: this.notices } );
+	this.notice( 'Saved' );
+	if( true === this.alert ) {
+		$notice( { title: $config.project_name + ' - ' + this.src, message: this.notices } );
+	}
 	return $return;
 };
 
@@ -226,9 +238,9 @@ $gulp.task( 'js', ( cb ) => vs_compile_all_js( 0 ) );
 $gulp.task( 'watch:scss', function( callback ) {
 	let $src;
 	for( $src in $config.scss ) {
-		$gulp.watch( $src, ( a, b ) => vs_compile_scss( $cwd( a[ 'path' ] ) ) );
+		$gulp.watch( $src, ( a, b ) => vs_compile_scss( $cwd( a[ 'path' ] ), true ) );
 		if( true !== isUndefined( $config.scss[ $src ][ 'watch' ] ) ) {
-			$gulp.watch( $config.scss[ $src ][ 'watch' ], () => vs_compile_scss( $src ) );
+			$gulp.watch( $config.scss[ $src ][ 'watch' ], () => vs_compile_scss( $src, true ) );
 		}
 	}
 	callback();
@@ -236,9 +248,9 @@ $gulp.task( 'watch:scss', function( callback ) {
 $gulp.task( 'watch:js', function( callback ) {
 	let $src;
 	for( $src in $config.js ) {
-		$gulp.watch( $src, ( a, b ) => vs_compile_js( $cwd( a[ 'path' ] ) ) );
+		$gulp.watch( $src, ( a, b ) => vs_compile_js( $cwd( a[ 'path' ] ), true ) );
 		if( true !== isUndefined( $config.js[ $src ][ 'watch' ] ) ) {
-			$gulp.watch( $config.js[ $src ][ 'watch' ], () => vs_compile_js( $src ) );
+			$gulp.watch( $config.js[ $src ][ 'watch' ], () => vs_compile_js( $src, true ) );
 		}
 	}
 	callback();
